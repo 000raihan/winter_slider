e401e368e5b00546dc88be61d9cd37fe4a3925b7
000raihan <000raihanahmad@gmail.com>
2023-01-10 15:27:08 +0600

    updated

--- src/views/Slider.js
+++ src/views/Slider.js
@@ -7,7 +7,7 @@ import './slider.css'
 const Slider = () => {
   return (
     <div className='slider'>
-        <Carousel infiniteLoop={true}>
+        <Carousel interval={3000} infiniteLoop={true}>
             <div className='img-container'>
                 <img src='slider_img/slider1.jpg'/>
             </div>
@@ -16,6 +16,39 @@ const Slider = () => {
             </div>
             <div className='img-container'>
                 <img src='slider_img/slider3.jpg'/>
+            </div>
+            <div className='img-container'>
+                <img src='slider_img/slider4.png'/>
+            </div>
+            <div className='img-container'>
+                <img src='slider_img/slider5.png'/>
+            </div>
+            <div className='img-container'>
+                <img src='slider_img/slider6.jpg'/>
+            </div>
+            <div className='img-container'>
+                <img src='slider_img/slider7.png'/>
+            </div>
+            <div className='img-container'>
+                <img src='slider_img/slider8.png'/>
+            </div>
+            <div className='img-container'>
+                <img src='slider_img/slider9.png'/>
+            </div>
+            <div className='img-container'>
+                <img src='slider_img/slider10.png'/>
+            </div>
+            <div className='img-container'>
+                <img src='slider_img/slider11.png'/>
+            </div>
+            <div className='img-container'>
+                <img src='slider_img/slider12.png'/>
+            </div>
+            <div className='img-container'>
+                <img src='slider_img/slider13.jpg'/>
+            </div>
+            <div className='img-container'>
+                <img src='slider_img/slider14.jpg'/>
             </div>
         </Carousel>
     </div>
