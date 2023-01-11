4d5c2631d56f63a4714a345eec4daf941ced3331
000raihan <000raihanahmad@gmail.com>
2023-01-11 13:17:54 +0600

    updated

--- src/views/Slider.js
+++ src/views/Slider.js
@@ -9,46 +9,60 @@ const Slider = () => {
     <div className='slider'>
         <Carousel interval={3000} infiniteLoop={true}>
             <div className='img-container'>
-                <img src='slider_img/1slider.jpg'/>
+                <img className='img1' src='slider_img/1slider.jpg'/>
+                <img className='img2' src='slider_img/1slider2.jpg'/>
             </div>
             <div className='img-container'>
-                <img src='slider_img/2slider.jpg'/>
+                <img className='img1' src='slider_img/2slider.jpg'/>
+                <img className='img2' src='slider_img/2slider2.jpg'/>
             </div>
             <div className='img-container'>
-                <img src='slider_img/3slider.jpg'/>
+                <img className='img1' src='slider_img/3slider.jpg'/>
+                <img className='img2' src='slider_img/3slider2.jpg'/>
             </div>
             <div className='img-container'>
-                <img src='slider_img/4slider.jpg'/>
+                <img className='img1' src='slider_img/4slider.jpg'/>
+                <img className='img2' src='slider_img/4slider2.jpg'/>
             </div>
             <div className='img-container'>
-                <img src='slider_img/5slider.jpg'/>
+                <img className='img1' src='slider_img/5slider.jpg'/>
+                <img className='img2' src='slider_img/5slider2.jpg'/>
             </div>
             <div className='img-container'>
-                <img src='slider_img/6slider.jpg'/>
+                <img className='img1' src='slider_img/6slider.jpg'/>
+                <img className='img2' src='slider_img/6slider2.jpg'/>
             </div>
             <div className='img-container'>
-                <img src='slider_img/7slider.png'/>
+                <img className='img1' src='slider_img/7slider.png'/>
+                <img className='img2' src='slider_img/7slider2.png'/>
             </div>
             <div className='img-container'>
-                <img src='slider_img/8slider.png'/>
+                <img className='img1' src='slider_img/8slider.png'/>
+                <img className='img2' src='slider_img/8slider2.png'/>
             </div>
             <div className='img-container'>
-                <img src='slider_img/9slider.png'/>
+                <img className='img1' src='slider_img/9slider.png'/>
+                <img className='img2' src='slider_img/9slider2.png'/>
             </div>
             <div className='img-container'>
-                <img src='slider_img/10slider.png'/>
+                <img className='img1' src='slider_img/10slider.png'/>
+                <img className='img2' src='slider_img/10slider2.png'/>
             </div>
             <div className='img-container'>
-                <img src='slider_img/11slider.png'/>
+                <img className='img1' src='slider_img/11slider.png'/>
+                <img className='img2' src='slider_img/11slider2.png'/>
             </div>
             <div className='img-container'>
-                <img src='slider_img/12slider.png'/>
+                <img className='img1' src='slider_img/12slider.png'/>
+                <img className='img2' src='slider_img/12slider2.png'/>
             </div>
             <div className='img-container'>
-                <img src='slider_img/13slider.png'/>
+                <img className='img1' src='slider_img/13slider.png'/>
+                <img className='img2' src='slider_img/13slider2.png'/>
             </div>
             <div className='img-container'>
-                <img src='slider_img/14slider.png'/>
+                <img className='img1' src='slider_img/14slider.png'/>
+                <img className='img2' src='slider_img/14slider2.png'/>
             </div>
         </Carousel>
     </div>
